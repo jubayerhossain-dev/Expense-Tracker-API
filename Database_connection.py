9b9806a95f0384183e4faeee8ce62bc84e8f6f0c
jubayerhossain0 <jubayercitdm@gmail.com>
2026-08-17 23:42:59 +0600

    Update pass

--- Database_connection.py
+++ Database_connection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_DATABASE_URL = 'postgresql://postgres.uxhpcqaqyyysvnzqpiem:Jubayer009@#@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
+SQL_DATABASE_URL = 'postgresql://postgres.uxhpcqaqyyysvnzqpiem:Expensetrackerapi@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
 engine = create_engine(SQL_DATABASE_URL)
 sessionlocal = sessionmaker(autoflush= False, autocommit= False, bind=engine)
